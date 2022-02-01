3e99d1443a097ca4018feae17b53c89f6c7a42c2
evann <evann.regnault@pm.me>
2022-02-01 21:54:48 +0100

    🩺 Ajout de Jest pour les tests

---
+++ backend-arktio/tests/foo.test.ts
@@ -0,0 +1,11 @@
+
+describe("Just a test file", () => {
+    it("1 should equal 1", () => {
+        expect(1).toBe(1)
+    })
+
+    it("1 should not equal 2", () => {
+        expect(1).not.toBe(2);
+    })
+
+});
