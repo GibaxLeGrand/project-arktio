6b29da9a6d95d7c8b5dc4a6437db3997c8485165
MOURAD SAMI <sami.mourad@etu.unistra.fr>
2022-02-26 20:00:31 +0100

    close socket on test

--- backend-arktio/tests/chat.test.ts
+++ backend-arktio/tests/chat.test.ts
@@ -2,27 +2,33 @@ import * as io from 'socket.io-client';
 import { ChatServer } from '../src/chat';
 
 describe("chat", () => {
-    let chatServer: ChatServer|null;
+    let chatServer: ChatServer | null;
 
     // Create the server
     beforeAll((done) => {
         chatServer = new ChatServer();
-        done()
-    })
+        done();
+    });
+
+    afterAll((done) => {
+        chatServer?.close();
+        done();
+    });
 
     test("chat server verification", (done) => {
-        const port: string | number = chatServer?.getPort() || -1;
+        let port: string | number = chatServer?.getPort() || -1;
         expect(port).toBeGreaterThan(-1);
-        const socket: any = io.connect(`http://localhost:${port}`, { query: { room: 'test' } });
 
-
+        let socket: any = io.connect(`http://localhost:${port}`, { query: { room: 'test' } });
 
         socket.on("message", ({player, message} : {player: string, message: string}) => {
             expect(player).toBe('testingPlayer');
             expect(message).toBe('test');
+            socket.close();
             done();
         });
 
         socket.emit("message", {player: 'testingPlayer', message: 'test'} );
     });
+
 });
