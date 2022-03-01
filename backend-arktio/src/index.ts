7d4155efedcf0e5130f940e0c0a157be99bda871
REGNAULT EVANN <evann.regnault@etu.unistra.fr>
2022-03-01 16:20:49 +0100

    ✨ Started route handling for login and registering

--- backend-arktio/src/index.ts
+++ backend-arktio/src/index.ts
@@ -1,8 +1,15 @@
 import { ChatServer } from './chat';
 import express from 'express';
 import * as http from 'http';
+import loginRoute from "./routes/loginRoute";
 
+// Init http server
 const app = express();
 const server = http.createServer(app);
 
+
+// Login Routes
+app.use("/login", loginRoute);
+
+// Execute listen so last thing to execute
 new ChatServer(server);
