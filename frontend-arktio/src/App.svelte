63cdc24b85875156ebb860e8743fe03ff4a1ef7a
Honeyxilia <lena.montenot@gmail.com>
2022-03-07 16:59:13 +0100

    Ajout formulaire pion, liste des joueur-euse-s

--- frontend-arktio/src/App.svelte
+++ frontend-arktio/src/App.svelte
@@ -2,9 +2,15 @@
   import Tailwindcss from "./Tailwindcss.svelte";
 
   enum Pions{
+    Aucun,
     Boite_conserve,
     Terre,
-
+    Plante,
+    Grain_cafe,
+    Bonnet,
+    Papillon,
+    Arosoir,
+    Nuage
   }
 
   class Player_lobby{
@@ -12,8 +18,10 @@
     name: string;
     pion: Pions;
 
-    constructor(present:boolean = false, ){
+    constructor(present:boolean = false, name: string = "", pion: Pions = Pions.Aucun){
       this.present = present;
+      this.name = name;
+      this.pion = pion;
     }
   }
 
@@ -23,10 +31,14 @@
 
   let name_lobby: string = "XXXX";
 
-  let player_1:Player_lobby = new Player_lobby();
-  let player_2:Player_lobby = new Player_lobby();
-  let player_3:Player_lobby = new Player_lobby();
-  let player_4:Player_lobby = new Player_lobby();
+  // Joueur-euse local
+  let player_local: Player_lobby = new Player_lobby();
+  let is_lobby_owner: boolean = false;
+
+  // Autres joueur-euses dans le lobby 
+  let player_1: Player_lobby = new Player_lobby();
+  let player_2: Player_lobby = new Player_lobby();
+  let player_3: Player_lobby = new Player_lobby();
 
 </script>
 
@@ -36,28 +48,39 @@
 <div class="logo">logo ici</div>
 
 <h id="lobby_name">Lobby {name_lobby}</h>
-  <div>
-    <label for="players">Joueur-euses :</label>
-      <ol id="players">
-        <li class="player"></li>
-        <li class="player"></li>
-        <li class="player"></li>
-        <li class="player"></li>
-      </ol>
-  </div>
+<label for="players">Joueur-euses :</label>  
+<div id="players">
+        <div class="player">{player_local.name}</div>
+        <div class="player">{player_1.name} </div>
+        <div class="player">{player_2.name} </div>
+        <div class="player">{player_3.name} </div>
+</div>
 
-  <div>
-    <input type="radio" value="boite-conserve">
-    <input type="radio" value="terre">
-    <input type="radio" value="plante">
-    <input type="radio" value="grain-cafe">
-    <input type="radio" value="bonnet">
-    <input type="radio" value="papillon">
-    <input type="radio" value="arosoir">
-    <input type="radio" value="nuage">
-  </div>
+  <form id="select_pion">
+    <input type="radio" id="boite-conserve" value="boite-conserve">
+    <label for="boite-conserve">Boite de conserve.</label>
+    <input type="radio" id="terre" value="terre">
+    <label for="terre">Terre.</label>
+    <input type="radio" id="plante" value="plante">
+    <label for="plante">Plante.</label>
+    <input type="radio" id="grain-cafe" value="grain-cafe">
+    <label for="grain-cafe">Grain de cafe.</label> <br>
 
-  <button aria-label="Lancer la partie" id="start_game">Commencer partie</button>
+    <input type="radio" id="bonnet" value="bonnet">
+    <label for="bonnet">Bonnet.</label>
+    <input type="radio" id="papillon" value="papillon">
+    <label for="papillon">Papillon.</label>
+    <input type="radio" id="arosoir" value="arosoir">
+    <label for="boite-conserve">Boite de conserve.</label>
+    <input type="radio" id="nuage" value="nuage">
+    <label for="boite-conserve">Boite de conserve.</label>
+  </form>
+
+  {#if is_lobby_owner}
+    <button aria-label="Lancer la partie" id="start_game">Commencer partie</button>
+  {:else}
+    <button aria-label="Lancer la partie" disabled>En attente du début de partie</button>
+  {/if}
 
   <footer>this is the footer</footer>
 </main>
