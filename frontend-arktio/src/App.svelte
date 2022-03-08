fb1fd2aa038bf84ed0080420487df46d8d53289d
Lena MONTENOT <orens.montenot@etu.unistra.fr>
2022-03-08 15:53:39 +0100

    Selection pion et mise en place avancees

--- frontend-arktio/src/App.svelte
+++ frontend-arktio/src/App.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
   import Tailwindcss from "./Tailwindcss.svelte";
 
-  enum Pions{
-    Aucun,
-    Boite_conserve,
-    Terre,
-    Plante,
-    Grain_cafe,
-    Bonnet,
-    Papillon,
-    Arosoir,
-    Nuage
-  }
+  let pions =[ 
+    {id: 0, text: "Aucun"},
+    {id: 1, text: "Boite de conserve"},
+    {id: 2, text: "Terre"},
+    {id: 3, text: "Plante"},
+    {id: 4, text: "Grain de café"},
+    {id: 5, text: "Bonnet"},
+    {id: 6, text: "Papillon"},
+    {id: 0, text: "Arosoir"},
+    {id: 0, text: "Nuage"}
+]
+  let pion_selectionne;
 
   class Player_lobby{
     present: boolean;
     name: string;
-    pion: Pions;
+    pion: number;
 
-    constructor(present:boolean = false, name: string = "", pion: Pions = Pions.Aucun){
+    constructor(present:boolean = false, name: string = ""){
       this.present = present;
       this.name = name;
-      this.pion = pion;
     }
+
+    set_pion(_pion: number){this.pion = _pion;}
   }
 
   const test = () => {
@@ -32,13 +34,15 @@
   let name_lobby: string = "XXXX";
 
   // Joueur-euse local
-  let player_local: Player_lobby = new Player_lobby();
+  let player_local: Player_lobby = new Player_lobby(true, "Honeyxilia");
   let is_lobby_owner: boolean = false;
 
   // Autres joueur-euses dans le lobby 
   let player_1: Player_lobby = new Player_lobby();
   let player_2: Player_lobby = new Player_lobby();
   let player_3: Player_lobby = new Player_lobby();
+
+  let players = [player_1, player_2, player_3];
 
 </script>
 
@@ -50,31 +54,19 @@
 <h id="lobby_name">Lobby {name_lobby}</h>
 <label for="players">Joueur-euses :</label>  
 <div id="players">
-        <div class="player">{player_local.name}</div>
-        <div class="player">{player_1.name} </div>
-        <div class="player">{player_2.name} </div>
-        <div class="player">{player_3.name} </div>
+        <div class="player">{player_local.present ? player_local.name : "---------"} </div>
+        <div class="player">{player_1.present ? player_1.name : "---------"} </div>
+        <div class="player">{player_2.present ? player_1.name : "---------"} </div>
+        <div class="player">{player_3.present ? player_1.name : "---------"} </div>
 </div>
 
-  <form id="select_pion">
-    <input type="radio" id="boite-conserve" value="boite-conserve">
-    <label for="boite-conserve">Boite de conserve.</label>
-    <input type="radio" id="terre" value="terre">
-    <label for="terre">Terre.</label>
-    <input type="radio" id="plante" value="plante">
-    <label for="plante">Plante.</label>
-    <input type="radio" id="grain-cafe" value="grain-cafe">
-    <label for="grain-cafe">Grain de cafe.</label> <br>
-
-    <input type="radio" id="bonnet" value="bonnet">
-    <label for="bonnet">Bonnet.</label>
-    <input type="radio" id="papillon" value="papillon">
-    <label for="papillon">Papillon.</label>
-    <input type="radio" id="arosoir" value="arosoir">
-    <label for="boite-conserve">Boite de conserve.</label>
-    <input type="radio" id="nuage" value="nuage">
-    <label for="boite-conserve">Boite de conserve.</label>
-  </form>
+  <select id="select_pion" bind:value ={pion_selectionne} on:change="{() => player_local.pion = pion_selectionne.id}">
+    {#each pions as pion}
+      <option value={pion}>{pion.text}</option>
+    {/each}
+  </select>
+  <p>Pion selectionné : {pion_selectionne ? pion_selectionne.text : "Aucun"}</p>
+   
 
   {#if is_lobby_owner}
     <button aria-label="Lancer la partie" id="start_game">Commencer partie</button>
@@ -89,6 +81,7 @@
   $color: #286143; // PS : c'était juste un test ou ça va servir ?
   $turquoise: #00a19a;
   $blanc: #ffffff;
+  $caramel: #ffd49a;
 
   main {
     @apply py-32;
@@ -122,6 +115,15 @@
     flex-direction: column;
     align-items: center;
     margin: 5vh;
+  }
+
+  #players{
+    color: $caramel;
+  }
+
+  .select_pion{
+    display: flex;
+    flex-direction: row;
   }
 
   .boutons {
