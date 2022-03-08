33f56c8ee055139961c23287acf5a7cd39dbc2e3
René Noel <rene.noel@etu.unistra.fr>
2022-03-08 11:32:26 +0100

    added register page

--- frontend-arktio/src/App.svelte
+++ frontend-arktio/src/App.svelte
@@ -11,24 +11,30 @@
 <Tailwindcss />
 <main />
 
-<h1 class="">Hello {name}!</h1>
-<p>
-  Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how
-  to build Svelte apps.
-</p>
-<div class="logo">logo ici</div>
+<div class="logo"><img alt="logo" src="logo.png" /></div>
 
 <div class="boutons">
-  <button id="player_name">nom du joueur</button>
-  <button id="game_name">nom de la partie</button>
-  <button id="pion_choice">choix du pion</button>
+
+  <form>
+    <label for="name">Nom du joeueur:</label>
+    <input type="text" id="name" name="name">
+    <label for="mail">Adresse mail:</label>
+    <input type="text" id="mail" name="mail">
+    <label for="password">Mot de passe:</label>
+    <input type="text" id="password" name="password">
+  </form>
+
   <button id="validate">valider</button>
+
 </div>
 
-<footer>this is the footer</footer>
+<footer>
+  <a href="url">condition générale d'utilisation</a>
+  <a href="non je déconne">Politique de cookie</a>
+  <a href="Qui est tu ?">Qui sommes nous ?</a>
+</footer>
 
 <style lang="scss">
-  $color: #286143; // PS : c'était juste un test ou ça va servir ?
   $turquoise: #00a19a;
   $blanc: #ffffff;
 
@@ -66,6 +72,39 @@
     margin: 5vh;
   }
 
+  img {  
+  max-width: 40%;  
+  height: auto;  
+  }  
+
+  form {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: space-around;    
+    color: $blanc;
+    block-size: 5vh;
+  }
+
+  label {
+    color: $blanc;
+    display: inherit;
+    justify-content: center;
+    text-align: center;
+    background-color: $turquoise;
+    font-weight: 100;
+    inline-size: 60vw;
+  }
+
+  input {
+    color: $blanc;
+    display: inherit;
+    justify-content: center;
+    text-align: right;
+    font-weight: 10;
+    inline-size: 30vw;
+  }
+
   .boutons {
     display: flex;
     flex-flow: column;
@@ -87,16 +126,19 @@
   }
 
   footer {
+    position: fixed;
+    padding: 10px 10px 0px 10px;
+    bottom: 0;
     text-align: center;
     font-family: Raleway;
     color: $blanc;
     background-color: $turquoise;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     width: 100%;
   }
 
+  footer>a{
+    padding: 5%;
+  }
   @media (min-width: 640px) {
     main {
       max-width: none;
