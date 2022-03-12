d844dcfbfccc48c6cc8e571e438e9b63816a8a56
Van PHAM <vpham@etu.unistra.fr>
2022-03-12 17:00:25 +0100

    correction retour de fonction

--- backend-arktio/src/bdd/functions/get_user.ts
+++ backend-arktio/src/bdd/functions/get_user.ts
@@ -1,7 +1,7 @@
 import { Users } from "../models/Users";
 
 // Fonction qui retourne un utilisateur dans la BDD par son id ou email et mot de passe.
-export async function getUser(id: number | string, password?: string): Promise<Users[]> {
+export async function getUser(id: number | string, password?: string): Promise<Users> {
     // On recherche par son ID, sinon par son email et son mot de passe. 
     if (typeof(id) == "number") {
 
@@ -9,7 +9,7 @@ export async function getUser(id: number | string, password?: string): Promise<U
             .select("*")
             .where("user_id", "=", `${id}`);
 
-        return user;
+        return user[0];
     } else {
 
         const user: Users[] = await Users.query()
@@ -17,6 +17,6 @@ export async function getUser(id: number | string, password?: string): Promise<U
             .where("user_email", "=", `${id}`)
             .where("user_password", "=", `${password}`);
 
-        return user;
+        return user[0];
     } 
 }
