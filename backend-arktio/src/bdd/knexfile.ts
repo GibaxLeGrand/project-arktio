96e37be10073f5393ab4e0298d3c391ba994b98e
Van PHAM <vpham@etu.unistra.fr>
2022-03-12 16:56:15 +0100

    ajout d'une bdd de test

--- backend-arktio/src/bdd/knexfile.ts
+++ backend-arktio/src/bdd/knexfile.ts
@@ -1,5 +1,6 @@
 import { Knex } from "knex";
 import dotenv from "dotenv";
+import { seed } from "./seeds/insert_user";
 
 /*
 	BUG : il faut un .env dans ./src pour lancer les migrations/seeds
@@ -24,6 +25,20 @@ const config: { [key: string]: Knex.Config } = {
 			user : process.env.USER_DEV,
 			password : process.env.PWD_DEV,
 			database : process.env.DATABASE_DEV
+		},
+		migrations: {
+			tableName: 'migrations'
+		}
+	},
+
+	testing: {
+		client: 'mysql2',
+		connection: {
+			host : process.env.HOST_TEST,
+			port : parseInt(process.env.PORT_TEST!),
+			user : process.env.USER_TEST,
+			password : process.env.PWD_TEST,
+			database : process.env.DATABASE_TEST
 		},
 		migrations: {
 			tableName: 'migrations'
