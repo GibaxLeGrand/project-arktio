cb7dd2cdcbd5efac56f490880ad964003ddc0a39
Nicolas Naouar--Niederst <naouarniederst@etu.unistra.fr>
2022-03-23 14:28:36 +0100

    :rotating_light: non used dependencies in comments

--- frontend-arktio/src/register.svelte
+++ frontend-arktio/src/register.svelte
@@ -1,59 +1,58 @@
 <script lang="ts">
-    import Tailwindcss from "./Tailwindcss.svelte";
-    import {register} from "./scripts/userScripts";
-    import {Route} from "tinro";
+  import Tailwindcss from "./Tailwindcss.svelte";
+  import { register } from "./scripts/userScripts";
+  import { Route } from "tinro";
 
-    let email = null;
-    let password = null;
-    let name = null;
-    let confirm_password = null;
+  let email = null;
+  let password = null;
+  let name = null;
+  let confirm_password = null;
 </script>
 
-<Tailwindcss/>
+<Tailwindcss />
 <main>
-        <div class="logo">
-            <img alt="logo" src="logo.png"/>
-        </div>
+  <div class="logo">
+    <img alt="logo" src="logo.png" />
+  </div>
 
-        <div class="boutons">
-            <form
-                    action="javascript:"
-                    on:submit={() => register(name, email, password, confirm_password)}
-            >
-                <label for="name">Nom du joueur:</label>
-                <input required type="text" id="name" name="name" bind:value={name}/>
+  <div class="boutons">
+    <form
+      action="javascript:"
+      on:submit={() => register(name, email, password, confirm_password)}
+    >
+      <label for="name">Nom du joueur:</label>
+      <input required type="text" id="name" name="name" bind:value={name} />
 
-                <label for="email">Adresse mail:</label>
-                <input required type="email" id="email" name="email" bind:value={email}/>
+      <label for="email">Adresse mail:</label>
+      <input required type="email" id="email" name="email" bind:value={email} />
 
-                <label for="password">Mot de passe:</label>
-                <input
-                        required
-                        type="password"
-                        id="password"
-                        name="password"
-                        bind:value={password}
-                />
+      <label for="password">Mot de passe:</label>
+      <input
+        required
+        type="password"
+        id="password"
+        name="password"
+        bind:value={password}
+      />
 
-                <label for="confirm_password">Confirmer le mot de passe:</label>
-                <input
-                        required
-                        type="password"
-                        id="confirm_password"
-                        name="confirm_password"
-                        bind:value={confirm_password}
-                />
+      <label for="confirm_password">Confirmer le mot de passe:</label>
+      <input
+        required
+        type="password"
+        id="confirm_password"
+        name="confirm_password"
+        bind:value={confirm_password}
+      />
 
-                <button id="validate" type="submit">S'inscrire</button>
-            </form>
-        </div>
+      <button id="validate" type="submit">S'inscrire</button>
+    </form>
+  </div>
 
-        <footer>
-            <a href="/url">condition générale d'utilisation</a>
-            <a href="/non je déconne">Politique de cookie</a>
-            <a href="/Qui est tu ?">Qui sommes nous ?</a>
-        </footer>
-
+  <footer>
+    <a href="/url">condition générale d'utilisation</a>
+    <a href="/non je déconne">Politique de cookie</a>
+    <a href="/Qui est tu ?">Qui sommes nous ?</a>
+  </footer>
 </main>
 
 <style lang="scss">
@@ -68,9 +67,9 @@
     margin: 0 auto;
   }
 
-  body {
-    background-color: $turquoise;
-  }
+  // body {
+  //   background-color: $turquoise;
+  // }
 
   // TODO pourquoi ça marche pas ça ?
 
@@ -80,13 +79,13 @@
     background-color: $turquoise;
   }
 
-  h1 {
-    color: $turquoise;
-    text-decoration-color: $blanc;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
+  // h1 {
+  //   color: $turquoise;
+  //   text-decoration-color: $blanc;
+  //   text-transform: uppercase;
+  //   font-size: 4em;
+  //   font-weight: 100;
+  // }
 
   .logo {
     color: $blanc;
