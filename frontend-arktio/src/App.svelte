9762e5ca1603262274ea290acff1fa75527e71d0
Nicolas Naouar--Niederst <naouarniederst@etu.unistra.fr>
2022-03-23 14:20:10 +0100

    :construction:  page de règles en cours de build

--- frontend-arktio/src/App.svelte
+++ frontend-arktio/src/App.svelte
@@ -1,103 +1,111 @@
 <script lang="ts">
-    import {Route, router} from "tinro";
-    import Login from "./login.svelte";
-    import Lobby from "./Lobby.svelte";
-    import Partie from "./partie.svelte";
-    import Register from "./register.svelte";
-    import {routerFetch} from "./scripts/fetchOverride";
-    import {base} from "./stores/locationStore";
-    import {get} from "svelte/store";
-    import {env} from "./scripts/envfile";
-    import {disconnect} from "./scripts/userScripts";
+  import { get } from "svelte/store";
+  import { Route, router } from "tinro";
+  import Lobby from "./Lobby.svelte";
+  import Login from "./login.svelte";
+  import Partie from "./partie.svelte";
+  import Register from "./register.svelte";
+  import { env } from "./scripts/envfile";
+  import { routerFetch } from "./scripts/fetchOverride";
+  import { disconnect } from "./scripts/userScripts";
+  import { base } from "./stores/locationStore";
+  import Regles from "./Regles.svelte";
 
-    router.mode.hash();
+  router.mode.hash();
 
-    base.set(env.root)
-    router.base(get(base));
+  base.set(env.root);
+  router.base(get(base));
 
-    enum RULES {
-        CONNECTED,
-        GUEST,
+  enum RULES {
+    CONNECTED,
+    GUEST,
+  }
+
+  let state: RULES = RULES.GUEST;
+
+  async function isAuth() {
+    const data = await routerFetch("/api/session/isAuth", { method: "GET" });
+    if ((await data.json()).authenticated) {
+      state = RULES.CONNECTED;
+    } else {
+      state = RULES.GUEST;
     }
+  }
 
-    let state: RULES = RULES.GUEST;
-
-    async function isAuth() {
-        const data = await routerFetch("/api/session/isAuth", {method: "GET"});
-        if ((await data.json()).authenticated) {
-            state = RULES.CONNECTED;
-        } else {
-            state = RULES.GUEST;
-        }
-    }
-
-    router.subscribe(() => {
-        isAuth()
-    })
+  router.subscribe(() => {
+    isAuth();
+  });
 </script>
 
 <main>
-    <Route path="/">
-        <div>
-            <a href="/">
-                <img
-                        src="https://cdn.discordapp.com/attachments/942433231599456307/952985982595104878/unknown.png"
-                        alt="Logo Arktio représentant un ours"
-                />
-            </a>
-        </div>
-        <div class="boutons">
-            {#if state === RULES.GUEST}
-                <button
-                        id="connexion"
-                        on:click={() => {
+  <Route path="/">
+    <div>
+      <a href="/">
+        <img
+          src="https://cdn.discordapp.com/attachments/942433231599456307/952985982595104878/unknown.png"
+          alt="Logo Arktio représentant un ours"
+        />
+      </a>
+    </div>
+    <div class="boutons">
+      {#if state === RULES.GUEST}
+        <button
+          id="connexion"
+          on:click={() => {
             router.goto("/login");
-          }}>Connexion
-                </button
-                >
-                <button
-                        id="inscription"
-                        on:click={() => {
+          }}
+          >Connexion
+        </button>
+        <button
+          id="inscription"
+          on:click={() => {
             router.goto("/register");
-          }}>Inscription
-                </button
-                >
-            {/if}
-            {#if [RULES.CONNECTED, RULES.GUEST].includes(state)}
-                <button
-                        id="create_join_party"
-                        on:click={() => {
+          }}
+          >Inscription
+        </button>
+      {/if}
+      {#if [RULES.CONNECTED, RULES.GUEST].includes(state)}
+        <button
+          id="create_join_party"
+          on:click={() => {
             router.goto("/partie");
-          }}>Créer / Rejoindre Partie
-                </button
-                >
-                <!-- // TODO fix chemin pour créer partie -->
-            {/if}
-            <button id="regles" title="afficher les règles">Règles</button>
-            {#if [RULES.CONNECTED].includes(state)}
-                <button
-                        id="disconnect"
-                        on:click={async () => {
+          }}
+          >Créer / Rejoindre Partie
+        </button>
+        <!-- // TODO fix chemin pour créer partie -->
+      {/if}
+      <button
+        id="regles"
+        title="afficher les règles"
+        on:click={() => router.goto("/Regles")}>Règles</button
+      >
+      {#if [RULES.CONNECTED].includes(state)}
+        <button
+          id="disconnect"
+          on:click={async () => {
             disconnect().then(isAuth);
-          }}>Déconnexion
-                </button
-                >
-            {/if}
-        </div>
-    </Route>
-    <Route path="/login">
-        <Login/>
-    </Route>
-    <Route path="/lobby">
-        <Lobby/>
-        // TODO peut être comme partie avec /:id
-    </Route>
-    <Route path="/register">
-        <Register/>
-    </Route>
-    <Route path="/partie/">
-        <Partie/>
-    </Route>
+          }}
+          >Déconnexion
+        </button>
+      {/if}
+    </div>
+  </Route>
+  <Route path="/login">
+    <Login />
+  </Route>
+  <Route path="/lobby">
+    <Lobby />
+    // TODO peut être comme partie avec /:id
+  </Route>
+  <Route path="/register">
+    <Register />
+  </Route>
+  <Route path="/partie/">
+    <Partie />
+  </Route>
+  <Route path="/Regles/">
+    <Regles />
+  </Route>
 </main>
 
 <style lang="scss">
@@ -154,15 +162,5 @@
     max-width: $size;
     max-height: $size;
     margin-top: 5%;
-  }
-
-  #rejoindre_creer_partie {
-    visibility: hidden;
-  }
-
-  @media (max-height: 800px) {
-    .boutons {
-      height: 50%;
-    }
   }
 </style>
