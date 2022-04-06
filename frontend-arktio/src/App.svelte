b7c9cd74bed83b7bfb0227a352747c71d949d517
Nicolas Naouar--Niederst <naouarniederst@etu.unistra.fr>
2022-04-06 18:43:17 +0200

    :building_construction: j'ai mis en commentaire le code de la page histoire de pas le perdre, j'ai fait un aperçu du plateau final si on utilise grid ( et qu'on s'en fiche que ce soit beau niveau code mdr ), je l'ai fait sur App.svelte pour avoir un aperçu direct sans devoir créer une partie etc

--- frontend-arktio/src/App.svelte
+++ frontend-arktio/src/App.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import { get } from "svelte/store";
   import { Route, router } from "tinro";
   import Lobby from "./Lobby.svelte";
@@ -72,7 +72,6 @@
           }}
           >Créer / Rejoindre Partie
         </button>
-        <!-- // TODO fix chemin pour créer partie -->
       {/if}
       <button
         id="regles"
@@ -162,5 +161,265 @@
     max-width: $size;
     max-height: $size;
     margin-top: 5%;
+  }
+</style> -->
+<script lang="ts">
+  const NB_CASES = 30;
+</script>
+
+<main>
+  <div class="case">
+    <div id="x1" class="cases_horizontal">X 1</div>
+    <div id="x2" class="cases_horizontal">X 2</div>
+    <div id="x3" class="cases_horizontal">X 3</div>
+    <div id="x4" class="cases_horizontal">X 4</div>
+    <div id="x5" class="cases_horizontal">X 5</div>
+    <div id="x6" class="cases_horizontal">X 6</div>
+    <div id="x7" class="cases_horizontal">X 7</div>
+    <div id="x8" class="cases_vertical">X 8</div>
+    <div id="x9" class="cases_vertical">X 9</div>
+    <div id="x10" class="cases_vertical">X 10</div>
+    <div id="x11" class="cases_vertical">X 11</div>
+    <div id="x12" class="cases_vertical">X 12</div>
+    <div id="x13" class="cases_vertical">X 13</div>
+    <div id="x14" class="cases_horizontal">X 14</div>
+    <div id="x15" class="cases_horizontal">X 15</div>
+    <div id="x16" class="cases_horizontal">X 16</div>
+    <div id="x17" class="cases_horizontal">X 17</div>
+    <div id="x18" class="cases_horizontal">X18</div>
+    <div id="x19" class="cases_vertical">X 19</div>
+    <div id="x20" class="cases_vertical">X 20</div>
+    <div id="x21" class="cases_vertical">X 21</div>
+    <div id="x22" class="cases_vertical">X 22</div>
+    <div id="x23" class="cases_vertical">X 23</div>
+    <div id="x24" class="cases_vertical">X 24</div>
+    <div id="x25" class="cases_vertical">X 25</div>
+    <div id="x26" class="cases_vertical">X 26</div>
+    <div id="x27" class="cases_horizontal">X 27</div>
+    <div id="x28" class="cases_horizontal">X 28</div>
+    <div id="x29" class="cases_horizontal">X 29</div>
+    <div id="x30" class="cases_horizontal">X 30</div>
+    <div id="conteneur">
+      "ÉVÉNEMENTS ( tu dois payer ...)"
+      <div id="image">image</div>
+      <button id="option1">options</button>
+      <button id="option2">options</button>
+      <button id="option3">options</button>
+      <button id="option4">options</button>
+    </div>
+  </div>
+</main>
+
+<style lang="scss">
+  $nb_cases_horizontal: 5;
+  $nb_cases_vertical: 8;
+  $taille_case: 1fr;
+
+  $turquoise: #00a19a;
+  $blanc: #ffffff;
+  $framboise: #ba105a;
+  $caramel: #ffd49a;
+  $turquoise_clair: #98d1cd;
+  $gris: #90908f;
+  $gris_fonce: #2c2c2c;
+  $font_arktio: Raleway;
+
+  main {
+    width: 100%;
+    height: 100%;
+    background-color: $turquoise_clair;
+
+    justify-content: center;
+    align-items: center;
+
+    font-size: xx-large;
+    color: #ffffff;
+  }
+
+  #plateau {
+    grid-template-areas:
+      "case cases_horizontal case"
+      "cases_vertical event cases_vertical"
+      "case cases_horizontal case";
+  }
+
+  #event {
+    grid-area: event;
+
+    width: $nb_cases_horizontal * $taille_case;
+    height: $nb_cases_vertical * $taille_case;
+  }
+
+  .case {
+    // grid-area: case;
+    // grid-template-columns: repeat(5, 10px) repeat(5, 10px);
+    // grid-template-rows: repeat(5, 10px) repeat(5, 10px);
+
+    // width: $taille_case;
+    // height: $taille_case;
+
+    width: 100%;
+    height: 100%;
+
+    border: solid $framboise;
+    border-width: 5px;
+
+    display: grid;
+    grid-gap: 5px;
+    grid-template-rows: repeat(10, $taille_case);
+    grid-template-columns: repeat(7, $taille_case);
+  }
+
+  .cases_horizontal {
+    // grid-area: cases_horizontal;
+
+    // background-color: blue;
+    width: $nb_cases_horizontal * $taille_case;
+    height: $taille_case;
+
+    border: dashed $gris_fonce;
+    border-width: 5px;
+
+    position: static;
+    justify-content: right;
+  }
+
+  // // 7 premiers éléments horizontaux
+  // .cases_horizontal:nth-child(-n + 7) {
+  //   background-color: #2c2c2c;
+  // }
+  // // 8 derniers éléments horizontaux ( 30-8 )
+  // .cases:nth-child(n + 22) {
+  //   background-color: deeppink;
+  // }
+
+  .cases_vertical {
+    // grid-area: cases_vertical;
+
+    width: $taille_case;
+    height: $nb_cases_vertical * $taille_case;
+
+    border: dashed $gris_fonce;
+    border-width: 5px;
+  }
+
+  #conteneur {
+    grid-column-start: 2;
+    grid-column-end: 7;
+    grid-row-start: 2;
+    grid-row-end: 10;
+
+    border: dashed red;
+    color: #2c2c2c;
+
+    // justify-self: center;
+    // align-self: center;
+    display: flexbox;
+  }
+
+  // TODO mettre les boutons en bas
+  button {
+    width: fit-content;
+    height: fit-content;
+    align-self: flex-end;
+    display: inline-flex;
+    justify-self: space-around;
+  }
+
+  #image {
+    align-self: center;
+  }
+
+  #x8 {
+    grid-column-start: 7;
+    grid-row-start: 2;
+  }
+  #x9 {
+    grid-column-start: 7;
+    grid-row-start: 3;
+  }
+  #x10 {
+    grid-column-start: 7;
+    grid-row-start: 4;
+  }
+  #x11 {
+    grid-column-start: 7;
+    grid-row-start: 5;
+  }
+  #x12 {
+    grid-column-start: 7;
+    grid-row-start: 6;
+  }
+  #x13 {
+    grid-column-start: 7;
+    grid-row-start: 7;
+  }
+  #x14 {
+    grid-column-start: 7;
+    grid-row-start: 8;
+  }
+  #x15 {
+    grid-column-start: 7;
+    grid-row-start: 9;
+  }
+  #x16 {
+    grid-column-start: 7;
+    grid-row-start: 10;
+  }
+  #x17 {
+    grid-column-start: 6;
+    grid-row-start: 10;
+  }
+  #x18 {
+    grid-column-start: 5;
+    grid-row-start: 10;
+  }
+  #x19 {
+    grid-column-start: 4;
+    grid-row-start: 10;
+  }
+  #x20 {
+    grid-column-start: 3;
+    grid-row-start: 10;
+  }
+  #x21 {
+    grid-column-start: 2;
+    grid-row-start: 10;
+  }
+  #x22 {
+    grid-column-start: 1;
+    grid-row-start: 10;
+  }
+  #x23 {
+    grid-column-start: 1;
+    grid-row-start: 9;
+  }
+  #x24 {
+    grid-column-start: 1;
+    grid-row-start: 8;
+  }
+  #x25 {
+    grid-column-start: 1;
+    grid-row-start: 7;
+  }
+  #x26 {
+    grid-column-start: 1;
+    grid-row-start: 6;
+  }
+  #x27 {
+    grid-column-start: 1;
+    grid-row-start: 5;
+  }
+  #x28 {
+    grid-column-start: 1;
+    grid-row-start: 4;
+  }
+  #x29 {
+    grid-column-start: 1;
+    grid-row-start: 3;
+  }
+  #x30 {
+    grid-column-start: 1;
+    grid-row-start: 2;
   }
 </style>
