8f67ba4bd3f7b533a2bf71664a420382d433e005
Sami MOURAD <mourads2001@gmail.com>
2022-05-02 13:42:58 +0200

    fix game test

--- backend-arktio/tests/game.test.ts
+++ backend-arktio/tests/game.test.ts
@@ -48,16 +48,16 @@ describe("game", () => {
         });
 
         await new Promise<void>(inLobby => {
-            clientSockets[0].emit("player information", usersUUID[0], ({ player1 }: {player1: PlayerJSON}) => {
+            clientSockets[0].emit("player information", usersUUID[0], ({ player }: {player: PlayerJSON}) => {
+                users.push(player);
                 clientSockets[0].emit("create lobby", ({ lobby } : { lobby: LobbyJSON }) => {
-                    users.push(player1);
                     let lobbyUUID = lobby.uuid;
                     
-                    clientSockets[1].emit("player information", usersUUID[1], ({ player2 }: { player2: PlayerJSON }) => {
-                        users.push(player2);
+                    clientSockets[1].emit("player information", usersUUID[1], ({ player }: { player: PlayerJSON }) => {
+                        users.push(player);
                         clientSockets[1].emit("join lobby", lobbyUUID, ({ valid, lobby } : { valid: boolean, lobby: LobbyJSON }) => {
-                            clientSockets[2].emit("player information", usersUUID[2], ({ player3 }: { player3: PlayerJSON }) => {
-                                users.push(player3);
+                            clientSockets[2].emit("player information", usersUUID[2], ({ player }: { player: PlayerJSON }) => {
+                                users.push(player);
                                 clientSockets[2].emit("join lobby", lobbyUUID, ({ valid, lobby } : { valid: boolean, lobby: LobbyJSON }) => {
                                     inLobby();
                                 });
